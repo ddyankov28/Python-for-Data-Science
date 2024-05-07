0d1563cffdc6038b8aa694024e377cb424e45ec2
Deyan Dyankov <deyandyankov23@yahoo.com>
2024-05-07 10:50:39 +0200

    ex01

--- Python-0-Starting/ex01/format_ft_time.py
+++ Python-0-Starting/ex01/format_ft_time.py
@@ -1,23 +1,25 @@
 import time
 
-# time.gmtime(0) returns the time as object
-
+# time.gmtime() returns the time as object     
 # time.time() return seconds since epoch
-
-
 
 monthEpoch = time.strftime("%B", time.gmtime(0))
 dayOfMonthEpoch = int(time.strftime("%d", time.gmtime(0)))
 yearEpoch = time.strftime("%Y", time.gmtime(0))
 secondsSinceEpoch = time.time()
-formattedtime = "{:,}".format(secondsSinceEpoch)
-print(formattedtime)
+formatSeconds = "{:,.4f}".format(secondsSinceEpoch)
+scientificSeconds = "{:.2e}".format(secondsSinceEpoch)
 
-
-print(f"Second since {monthEpoch} {dayOfMonthEpoch}, {yearEpoch}: {secondsSinceEpoch}")
+print(f"Seconds since {monthEpoch} {dayOfMonthEpoch}, "
+    f"{yearEpoch}: {formatSeconds} or {scientificSeconds} "
+    f"in scientific notation")
+print(time.strftime("%b %d %Y", time.gmtime()))
 
 
 
 
 # resource:
 #     - https://docs.python.org/3/library/time.html#module-time
+#     - https://www.w3schools.com/python/ref_string_format.asp
+#     - https://www.w3schools.com/python/python_datetime.asp
+#     - https://www.geeksforgeeks.org/python-time-module/
