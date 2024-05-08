91347a19ace6bf5382438812f06406a2bfc4e2f5
Deyan Dyankov <deyandyankov23@yahoo.com>
2024-05-08 10:33:04 +0200

    ex04

---
+++ Python-0-Starting/ex04/whatis.py
@@ -0,0 +1,20 @@
+import sys
+
+if len(sys.argv) > 1:
+    if len(sys.argv) > 2:
+        print("AssertionError: more than one argument is provided")
+    elif sys.argv[1].isdigit():
+        if  int(sys.argv[1]) % 2 == 0:
+            print("I'm Even")
+        elif int(sys.argv[1]) % 2 != 0:
+            print("I'm Odd")
+    else:
+        print("AssertionError: argument is not an integer")
+
+
+
+
+# They are actually 3 most common ways to get the args
+# 1.sys.argv 2.getopt module 3.argparse module
+# resource:
+#       - https://www.geeksforgeeks.org/command-line-arguments-in-python/
