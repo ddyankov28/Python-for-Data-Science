d89a3d75633dbae3015faf90d921b0fd70f71c34
Deyan Dyankov <deyandyankov23@yahoo.com>
2024-05-08 15:38:12 +0200

    ex05

---
+++ Python-0-Starting/ex05/building.py
@@ -0,0 +1,6 @@
+
+
+# resource:
+#       - https://realpython.com/if-name-main-python/
+#       - https://betterstack.com/community/questions/what-does-if-name-is-main-do-in-python/
+#       - https://www.freecodecamp.org/news/if-name-main-python-example/
