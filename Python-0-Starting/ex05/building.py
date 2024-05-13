181a7d2b3cbdfa8770f22c5ba6f2913a64f8f422
Deyan Dyankov <deyandyankov23@yahoo.com>
2024-05-13 11:10:37 +0200

    flake8

--- Python-0-Starting/ex05/building.py
+++ Python-0-Starting/ex05/building.py
@@ -1,6 +1,15 @@
+def main():
+    '''
+    The main function here
+    '''
 
+
+if __name__ == "__main__":
+    main()
 
 # resource:
 #       - https://realpython.com/if-name-main-python/
-#       - https://betterstack.com/community/questions/what-does-if-name-is-main-do-in-python/
-#       - https://www.freecodecamp.org/news/if-name-main-python-example/
+#       - https://betterstack.com/community/questions/
+#           what-does-if-name-is-main-do-in-python/
+#       - https://www.freecodecamp.org/news/if-name-main-python-example/git
+#       - https://www.youtube.com/watch?v=0YUdYk5E-w4&ab_channel=Programiz
