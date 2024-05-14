2ae5e6989ed812324baa0278b66921ddd1b2b2d7
Deyan Dyankov <deyandyankov23@yahoo.com>
2024-05-14 15:39:25 +0200

    ex07

---
+++ Python-0-Starting/ex07/sos.py
@@ -0,0 +1,64 @@
+import sys
+
+NESTED_MORSE = {    " ": "/ ",
+                    "A": ".- ",
+                    "B": "-... ",
+                    "C": "-.-. ",
+                    "D": "-.. ",
+                    "E": ". ",
+                    "F": "..-. ",
+                    "G": "--. ",
+                    "H": ".... ",
+                    "I": ".. ",
+                    "J": ".--- ",
+                    "K": "-.- ",
+                    "L": ".-.. ",
+                    "M": "-- ",
+                    "N": "-. ",
+                    "O": "--- ",
+                    "P": ".--. ",
+                    "Q": "--.- ",
+                    "R": ".-. ",
+                    "S": "... ",
+                    "T": "- ",
+                    "U": "..- ",
+                    "V": "...- ",
+                    "W": ".-- ",
+                    "X": "-..- ",
+                    "Y": "-.-- ",
+                    "Z": "--.. ",
+                    "1": ".---- ",
+                    "2": "..--- ",
+                    "3": "...-- ",
+                    "4": "....- ",
+                    "5": "..... ",
+                    "6": "-.... ",
+                    "7": "--... ",
+                    "8": "---.. ",
+                    "9": "----. ",
+                    "0": "----- "
+}
+
+def checkInput(S):
+    '''
+    Checks the input string if contains only space and alphanumeric chars
+    '''
+    for char in S:
+        if not char.isalnum() and not char == ' ':
+            return False
+        else:
+            continue
+    return True
+
+def main():
+    if len(sys.argv[1:]) != 1:
+        print("AssertionError: the arguments are bad")
+    else:
+        if checkInput(sys.argv[1]):
+            print("All correct")
+        else:
+            print("AssertionError: the arguments are bad")
+
+
+if __name__ == "__main__":
+    main()
