ab99c313e6e399e0959a5bdcab52de27498938fb
Deyan Dyankov <deyandyankov23@yahoo.com>
2024-05-21 15:07:14 +0200

    bmi index

---
+++ Python-1-Array/ex00/give_bmi.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def give_bmi(height: list[int | float], weight: list[int | float]) -> list[int | float]:
+
+
+# resources
+# BMI = Body mass index
+# https://numpy.org/doc/stable/user/absolute_beginners.html
+# BMI = WEIGHT / (HEIGHT * HEIGHT)
