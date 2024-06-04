68ea7dee377c3cb29303bf800cafd59edaf0a4f9
ddyankov28 <deyandyankov23@yahoo.com>
2024-06-04 15:17:54 +0200

    ex03

---
+++ Python-2-DataTable/ex03/load_csv.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+
+def load(path: str) -> pd.DataFrame:
+    '''
+    Reads .CSV file, prints the dimensions of the data set and
+    returns it as a Pandas DataFrame object
+    -------
+    @param
+        - path: str -> the path of the file
+    -------
+    @rtype
+        - Pandas DataFrame
+    -------
+    @return
+        - The data from file as a Pandas DataFrame ojbect
+    '''
+    try:
+        dataFrame = pd.read_csv(path)
+    except Exception:
+        print("Error: Path is bad or has bad format")
+        return None
+    print(f"Loading dataset of dimensions {dataFrame.shape}")
+    return dataFrame
+
+# resources
+# - https://www.w3schools.com/python/pandas/pandas_dataframes.asp
+# - https://www.w3schools.com/python/pandas/pandas_csv.asp
---
+++ Python-2-DataTable/ex03/projection_life.py
@@ -0,0 +1,15 @@
+from load_csv import load
+
+
+def main():
+    '''
+    Loads the 2 files in the repo and displays the projection of life
+    expectancy in relation to the gross national product of the year
+    1900 for each country. The graph has a title, a legend for each
+    axis and a legend for each graph
+    '''
+    print("To Do")
+    
+
+if __name__ == "__main__":
+    main()
