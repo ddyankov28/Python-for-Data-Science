f998849f5d064bab60992c179e06095b2a7d803f
ddyankov28 <deyandyankov23@yahoo.com>
2024-06-12 21:38:27 +0200

    ex04

--- Python-3-OOP/ex04/ft_calculator.py
+++ Python-3-OOP/ex04/ft_calculator.py
@@ -1,1 +1,14 @@
 class calculator:
+    '''Docstring calculator Class'''
+    @staticmethod
+    def dotproduct(V1: list[float], V2: list[float]) -> None:
+        for number in V1:
+            
+
+
+# NOTES:
+# - The dot product (also known as the scalar product) is an operation
+#   that takes two vectors and returns a scalar (a single number).
+
+# resources
+# -https://www.kdnuggets.com/8-built-in-python-decorators-to-write-elegant-code
