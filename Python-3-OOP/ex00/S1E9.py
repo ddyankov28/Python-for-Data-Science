f1ce42dac624570c5c41523479fffebb59feaeb8
Deyan Dyankov <deyandyankov23@yahoo.com>
2024-06-12 11:15:38 +0200

    ex00 done

--- Python-3-OOP/ex00/S1E9.py
+++ Python-3-OOP/ex00/S1E9.py
@@ -1,33 +1,33 @@
 from abc import ABC, abstractmethod
 
+
 class Character(ABC):
-    '''
-    Abstract Class Character
-    '''
+    '''Abstract Class Character'''
     @abstractmethod
     def __init__(self, first_name, is_alive=True):
-        '''
-        Object initialization with name 
-        and var for life status
-        '''
+        '''Doc for the Constructor of Abstract Class'''
         self.first_name = first_name
         self.is_alive = is_alive if is_alive is not True else is_alive
-        
+
+    def die(self):
+        '''Doc for the Die method'''
+        self.is_alive = False
+
 
 class Stark(Character):
-    '''
-    asd
-    '''
+    '''Doc for the Stark Class'''
     def __init__(self, first_name, is_alive=True):
-        '''
-        abc
-        '''
+        '''Doc for the Constructor of Stark Class'''
         super().__init__(first_name, is_alive)
 
 # NOTES:
 # - The __init__() function is called automatically every time
 #              the class is being used to create a new object.
+# - The self parameter is a reference to the current instance
+#              of the class, and is used to access variables that
+#              belong to the class.
 
 # resources
+# - https://www.w3schools.com/python/python_classes.asp
 # - https://docs.python.org/3/library/abc.html
 # - https://www.geeksforgeeks.org/abstract-classes-in-python/
--- Python-3-OOP/ex00/tester.py
+++ Python-3-OOP/ex00/tester.py
@@ -2,3 +2,15 @@ from S1E9 import Character, Stark
 
 Ned = Stark("Ned")
 print(Ned.__dict__)
+print(Ned.is_alive)
+Ned.die()
+print(Ned.is_alive)
+print(Ned.__doc__)
+print(Ned.__init__.__doc__)
+print(Ned.die.__doc__)
+print("---")
+Lyanna = Stark("Lyanna", False)
+print(Lyanna.__dict__)
+
+# Test that Abstract Class can not be instantiated
+hodor = Character("hodor")
