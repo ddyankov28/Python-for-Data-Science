3da7cfff7102c0581a73e27465d303dd6e8395e9
Deyan Dyankov <deyandyankov23@yahoo.com>
2024-06-17 09:08:03 +0200

    done

--- Python-3-OOP/ex00/tester.py
+++ Python-3-OOP/ex00/tester.py
@@ -1,16 +1,25 @@
 from S1E9 import Character, Stark
 
-Ned = Stark("Ned")
-print(Ned.__dict__)
-print(Ned.is_alive)
-Ned.die()
-print(Ned.is_alive)
-print(Ned.__doc__)
-print(Ned.__init__.__doc__)
-print(Ned.die.__doc__)
-print("---")
-Lyanna = Stark("Lyanna", False)
-print(Lyanna.__dict__)
 
-# Test that Abstract Class can not be instantiated
-hodor = Character("hodor")
+def main():
+    try:
+        Ned = Stark("Ned")
+        print(Ned.__dict__)
+        print(Ned.is_alive)
+        Ned.die()
+        print(Ned.is_alive)
+        print(Ned.__doc__)
+        print(Ned.__init__.__doc__)
+        print(Ned.die.__doc__)
+        print("---")
+        Lyanna = Stark("Lyanna", False)
+        print(Lyanna.__dict__)
+
+        # Test that Abstract Class can not be instantiated
+        hodor = Character("hodor")
+    except Exception as e:
+        print("Error: ", e)
+
+
+if __name__ == "__main__":
+    main()
