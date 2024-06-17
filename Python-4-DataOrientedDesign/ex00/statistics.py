241c7fa28364a3e0a6593b262a89c0f43533e49c
Deyan Dyankov <deyandyankov23@yahoo.com>
2024-06-17 13:36:51 +0200

    ex00

--- Python-4-DataOrientedDesign/ex00/statistics.py
+++ Python-4-DataOrientedDesign/ex00/statistics.py
@@ -1,5 +1,22 @@
-def ft_statistics(*args: Any, **kwargs: Any) -> None:
-    
+def ft_statistics(*args: any, **kwargs: any) -> None:
+    '''
+    Takes *args a quantity of unknown number and makes the
+    Mean, Median, Quartile (25% and 75%), Standard Deviation and
+    Variance according to the **kwargs (Keyword Arguments)
+    -------
+    @param
+        - *args: any -> unknow amount of numeric values
+        - **kwargs: any -> unknown amount of keyword arguments
+    -------
+    @rtype
+        - None
+    -------
+    @return
+        - None
+    ''' 
+    print(args)
+    print("----")
+    print(kwargs)    
 
 # NOTES:
 # - If you don't know how many arguments that will be passed into a function
@@ -13,3 +30,4 @@ def ft_statistics(*args: Any, **kwargs: Any) -> None:
 
 # resources
 # - https://www.w3schools.com/python/gloss_python_function_arbitrary_arguments.asp
+# - https://www.w3schools.com/python/python_ml_mean_median_mode.asp
