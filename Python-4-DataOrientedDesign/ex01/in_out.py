10df8964d443dcf8738e1191e7357465e88d5448
Deyan Dyankov <deyandyankov23@yahoo.com>
2024-06-19 11:32:26 +0200

    ex01

---
+++ Python-4-DataOrientedDesign/ex01/in_out.py
@@ -0,0 +1,17 @@
+def square(x: int | float) -> int | float:
+    return x * x
+    
+
+
+def pow(x: int | float) -> int | float:
+    return pow()
+
+
+
+def outer(x: int | float, function) -> object:
+        count = 0
+    def inner() -> float:
+        
+
+# resources
+# - https://www.geeksforgeeks.org/python-inner-functions/
---
+++ Python-4-DataOrientedDesign/ex01/tester.py
@@ -0,0 +1,5 @@
+from in_out import outer
+from in_out import square
+from in_out import pow
+
+my_counter = outer(3, square)
