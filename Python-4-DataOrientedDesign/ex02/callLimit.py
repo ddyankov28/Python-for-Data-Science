152732740943dac73744128ad4feb1cd08d27c8f
ddyankov28 <deyandyankov23@yahoo.com>
2024-06-23 12:29:15 +0200

    flake8 ex02

--- Python-4-DataOrientedDesign/ex02/callLimit.py
+++ Python-4-DataOrientedDesign/ex02/callLimit.py
@@ -1,7 +1,21 @@
 def callLimit(limit: int):
+    '''
+    This is the main function that is acting like the home of the
+    decorator and accepts an int limit that later will be checked
+    how often the function passed will be called
+    '''
     count = 0
+
     def callLimiter(function):
+        '''
+        The actual decorator function. Returns a new function that
+        wraps the function passed as an argument
+        '''
         def limit_function(*args: any, **kwds: any):
+            '''
+            The actual function to check how often the function passed
+            was called. Raises exception if the count exceeds the limit
+            '''
             try:
                 nonlocal count
                 if count == limit:
@@ -21,5 +35,5 @@ def callLimit(limit: int):
 #       additional functionality to the original code without
 #       changing its core logic
 
-# resources 
+# resources
 # - https://www.geeksforgeeks.org/function-wrappers-in-python/
--- Python-4-DataOrientedDesign/ex02/tester.py
+++ Python-4-DataOrientedDesign/ex02/tester.py
@@ -1,12 +1,15 @@
 from callLimit import callLimit
 
+
 @callLimit(3)
 def f():
     print("f()")
 
+
 @callLimit(1)
 def g():
     print("g()")
+
 
 for i in range(3):
     f()
